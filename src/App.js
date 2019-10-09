bab703ba26b6c5c5ce676c54bbbdb9d5bdc9d218
leejaewoong <dreamsctv@gmail.com>
2019-10-09 22:07:44 +0900

    movie_app

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-function App() {
+class App extends Component {
+  state = {};
 
-  const movies = [
-    {
-      title : "Drug War",
-      poster : "https://post-phinf.pstatic.net/MjAxODA1MjJfNjQg/MDAxNTI2OTYzMjEyMDI4.ZfY5-zRI0q9pVtj127kKlCEbBYpsrDrjdndFcxq-xCwg.pBLORyz1ildVHujcApZ1hzYb1yhW2tV-nb1Tj2T3aeUg.JPEG/0.1.0_%283%29.jpg?type=w1200"
-    },
-    {
-      title : "Drug War",
-      poster : "https://post-phinf.pstatic.net/MjAxODA1MjJfNjQg/MDAxNTI2OTYzMjEyMDI4.ZfY5-zRI0q9pVtj127kKlCEbBYpsrDrjdndFcxq-xCwg.pBLORyz1ildVHujcApZ1hzYb1yhW2tV-nb1Tj2T3aeUg.JPEG/0.1.0_%283%29.jpg?type=w1200"
-    },
-    {
-      title : "Drug War",
-      poster : "https://post-phinf.pstatic.net/MjAxODA1MjJfNjQg/MDAxNTI2OTYzMjEyMDI4.ZfY5-zRI0q9pVtj127kKlCEbBYpsrDrjdndFcxq-xCwg.pBLORyz1ildVHujcApZ1hzYb1yhW2tV-nb1Tj2T3aeUg.JPEG/0.1.0_%283%29.jpg?type=w1200"
-    },
-    {
-      title : "Drug War",
-      poster : "https://post-phinf.pstatic.net/MjAxODA1MjJfNjQg/MDAxNTI2OTYzMjEyMDI4.ZfY5-zRI0q9pVtj127kKlCEbBYpsrDrjdndFcxq-xCwg.pBLORyz1ildVHujcApZ1hzYb1yhW2tV-nb1Tj2T3aeUg.JPEG/0.1.0_%283%29.jpg?type=w1200"
-    }
+  componentDidMount() {
+    this._getMoves();
+  }
 
-  ]
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      console.log(movie);
+      return <Movie title={movie.title} poster={movie.large_cover_image} key={index} />;
+    });
+    return movies;
+  };
+
+  _getMoves = async () => {
+    const movies = await this._callApi();
+    this.setState({
+      movies,
+    });
+  };
+
+  _callApi = () => {
+    return fetch('https://yts.lt/api/v2/list_movies.json?sort_by=rating')
+      .then(response => response.json())
+      .then(json => json.data.movies)
+      .catch(err => console.log(err));
+  };
+
   //map함수로 movies배열을movie변수에 넣어줌
   //key를 사용해서 고유의 값을 부여할 수 있음
-  return (
-    <div className="App">
-      {movies.map((movie, index) => {
-        return <Movie title={movie.title} poster={movie.poster} key={index}/>
-      })}
-    </div>
-  );
+  render() {
+    return <div className="App">{this.state.movies ? this._renderMovies() : 'Loading'}</div>;
+  }
 }
 
 export default App;
